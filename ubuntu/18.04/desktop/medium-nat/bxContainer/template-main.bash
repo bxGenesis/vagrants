9fdea8e3c78bffe760cd0a022be216db32c627a9
Mohsen-atHuawei BANAN <mohsen.banan@huawei.com>
2019-01-02 15:56:35 -0800

    Added guestBootstrap direct execution feature.

--- ubuntu/18.04/desktop/medium-nat/bxContainer/template-main.bash
+++ ubuntu/18.04/desktop/medium-nat/bxContainer/template-main.bash
@@ -1,9 +1,12 @@
       cat  << _EOF_
  ######### PHASE 0: Parameters Specification
 _EOF_
-      genesisUserName=vagrant
-      genesisUserGroup=vagrant
+      genesisUserName=$(id -u -n)
+      genesisUserGroup=$(id -g -n)
       genesisUserPasswd=vagrant
+      bashPath=$(which bash)
+      bxBootstrapBase="/var/bxBootstrap"
+      
       genesisGitRepoCloneCmnd="git clone xxx"
       genesisGitRepoEntryCmnd="./xx/yy"
 
@@ -17,17 +20,22 @@ _EOF_
       # sudo usermod -aG wheel i2user
       # sudo echo 'i2user ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
+      sudo apt install git
+
+      sudo mkdir -p $bxBootstrapBase
+      sudo chown $genesisUserName:$genesisUserGroup $bxBootstrapBase
+
       cat  << _EOF_
  ######### PHASE 2: Bring Over genesisClone -- Running As $genesisUserName
 _EOF_
 
-      sudo apt install git
 
-      sudo -u $genesisUserName /usr/bin/bash << _EOF_genessisUserName_
+      sudo -u $genesisUserName $bashPath << _EOF_genessisUserName_
 
-      cd ~$genesisUserName
-      mkdir -p ~$genesisUserName/gitRepos
-      cd ~$genesisUserName/gitRepos
+      mkdir -p $bxBootstrapBase/gitRepos
+      cd $bxBootstrapBase/gitRepos
+
+      #cd ~$genesisUserName/gitRepos
       #$genesisGitRepoCloneCmnd
 
       #$genesisGitRepoEntryCmnd
